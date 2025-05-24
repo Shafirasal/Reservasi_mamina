58ae7331dee1a1f793dc15d09a1092b3e57043e5
Oddis Nur Alifathur Razaaq <oddisnur@gmail.com>
2025-05-24 14:02:56 +0700

    Oddis

---
+++ app/Database/Seeds/PelangganSeeder.php
@@ -0,0 +1,216 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class PelangganSeeder extends Seeder
+{
+    public function run()
+    {
+        $data = [
+            [
+                'nama_pelanggan' => 'Aurora',
+                'nama_ortu_pelanggan' => 'Pingkan Apriliananda',
+                'usia_pelanggan' => '4 bulan',
+                'no_hp_pelanggan' => '082244136757',
+                'kota_pelanggan' => 'Malang',
+                'status_member' => 1
+            ],
+            [
+                'nama_pelanggan' => 'Ayden',
+                'nama_ortu_pelanggan' => 'Nabila',
+                'usia_pelanggan' => '10 bulan',
+                'no_hp_pelanggan' => '085954500549',
+                'kota_pelanggan' => 'Malang',
+                'status_member' => 1
+            ],
+            [
+                'nama_pelanggan' => 'Arcelio',
+                'nama_ortu_pelanggan' => 'Fani',
+                'usia_pelanggan' => '4 bulan 11 hari',
+                'no_hp_pelanggan' => '08966034000',
+                'kota_pelanggan' => 'Surabaya',
+                'status_member' => 0
+            ],
+            [
+                'nama_pelanggan' => 'Ara',
+                'nama_ortu_pelanggan' => 'Ikhtiarani',
+                'usia_pelanggan' => '7 bulan',
+                'no_hp_pelanggan' => '089602091799',
+                'kota_pelanggan' => 'Malang',
+                'status_member' => 0
+            ],
+            [
+                'nama_pelanggan' => 'Reyhito',
+                'nama_ortu_pelanggan' => 'Devi',
+                'usia_pelanggan' => '14 bulan',
+                'no_hp_pelanggan' => '085936161479',
+                'kota_pelanggan' => 'Malang',
+                'status_member' => 0
+            ],
+            [
+                'nama_pelanggan' => 'Asmira',
+                'nama_ortu_pelanggan' => 'Yulia Wahyu',
+                'usia_pelanggan' => '7 bulan',
+                'no_hp_pelanggan' => '0881082581358',
+                'kota_pelanggan' => 'Malang',
+                'status_member' => 0
+            ],
+            [
+                'nama_pelanggan' => 'Danial',
+                'nama_ortu_pelanggan' => 'Fauzia',
+                'usia_pelanggan' => '14 bulan',
+                'no_hp_pelanggan' => '082143400021',
+                'kota_pelanggan' => 'Malang',
+                'status_member' => 0
+            ],
+            [
+                'nama_pelanggan' => 'Enza',
+                'nama_ortu_pelanggan' => 'Lia',
+                'usia_pelanggan' => '9 bulan',
+                'no_hp_pelanggan' => '088806891991',
+                'kota_pelanggan' => 'Malang',
+                'status_member' => 1
+            ],
+            [
+                'nama_pelanggan' => 'Rayden',
+                'nama_ortu_pelanggan' => 'Rizka Hikma',
+                'usia_pelanggan' => '7 bulan',
+                'no_hp_pelanggan' => '088203589798',
+                'kota_pelanggan' => 'Malang',
+                'status_member' => 1
+            ],
+            [
+                'nama_pelanggan' => 'Keenan',
+                'nama_ortu_pelanggan' => 'Inas Fadhilah',
+                'usia_pelanggan' => '7 bulan',
+                'no_hp_pelanggan' => '087856897325',
+                'kota_pelanggan' => 'Malang',
+                'status_member' => 0
+            ],
+            [
+                'nama_pelanggan' => 'Fazzal',
+                'nama_ortu_pelanggan' => 'Nana',
+                'usia_pelanggan' => '2 bulan',
+                'no_hp_pelanggan' => '081932702457',
+                'kota_pelanggan' => 'Kota Malang',
+                'status_member' => 1
+            ],
+            [
+                'nama_pelanggan' => 'Lituhayu',
+                'nama_ortu_pelanggan' => 'Dewa',
+                'usia_pelanggan' => '8 bulan',
+                'no_hp_pelanggan' => '082331533838',
+                'kota_pelanggan' => 'Kota malang',
+                'status_member' => 0
+            ],
+            [
+                'nama_pelanggan' => 'Emir',
+                'nama_ortu_pelanggan' => 'Savitri',
+                'usia_pelanggan' => '9 bulan',
+                'no_hp_pelanggan' => '081330136125',
+                'kota_pelanggan' => 'Malang',
+                'status_member' => 0
+            ],
+            [
+                'nama_pelanggan' => 'Muhammad Ibrahim Kanzun',
+                'nama_ortu_pelanggan' => 'Renna Ressy Dewi',
+                'usia_pelanggan' => '8 bulan',
+                'no_hp_pelanggan' => '081932678338',
+                'kota_pelanggan' => 'Malang',
+                'status_member' => 0
+            ],
+            [
+                'nama_pelanggan' => 'Adzkiya Maira Azzahra',
+                'nama_ortu_pelanggan' => 'Ukik Agustina',
+                'usia_pelanggan' => '2 Bulan',
+                'no_hp_pelanggan' => '082228690187',
+                'kota_pelanggan' => 'Kediri',
+                'status_member' => 0
+            ],
+            [
+                'nama_pelanggan' => 'Raksaka',
+                'nama_ortu_pelanggan' => 'Rafika',
+                'usia_pelanggan' => '7 bulan',
+                'no_hp_pelanggan' => '082260255780',
+                'kota_pelanggan' => 'Nganjuk',
+                'status_member' => 0
+            ],
+            [
+                'nama_pelanggan' => 'Gendhis',
+                'nama_ortu_pelanggan' => 'Sakina M',
+                'usia_pelanggan' => '5 bulan',
+                'no_hp_pelanggan' => '085735112041',
+                'kota_pelanggan' => 'Pakis',
+                'status_member' => 0
+            ],
+            [
+                'nama_pelanggan' => 'Fia',
+                'nama_ortu_pelanggan' => 'Meilania Nanda N.A',
+                'usia_pelanggan' => '17 bulan',
+                'no_hp_pelanggan' => '856045511191',
+                'kota_pelanggan' => 'Malang',
+                'status_member' => 1
+            ],
+            [
+                'nama_pelanggan' => 'Felix Heryanto',
+                'nama_ortu_pelanggan' => 'Mutia',
+                'usia_pelanggan' => '15 bulan',
+                'no_hp_pelanggan' => '081216519959',
+                'kota_pelanggan' => 'Malang',
+                'status_member' => 0
+            ],
+            [
+                'nama_pelanggan' => 'Chio',
+                'nama_ortu_pelanggan' => 'Savira',
+                'usia_pelanggan' => '3 bulan',
+                'no_hp_pelanggan' => '083820224433',
+                'kota_pelanggan' => 'Malang',
+                'status_member' => 0
+            ],
+            [
+                'nama_pelanggan' => 'Gala',
+                'nama_ortu_pelanggan' => 'Lolita',
+                'usia_pelanggan' => '3 bulan',
+                'no_hp_pelanggan' => '085603500559',
+                'kota_pelanggan' => 'Malang',
+                'status_member' => 1
+            ],
+            [
+                'nama_pelanggan' => 'Azula',
+                'nama_ortu_pelanggan' => 'Ita',
+                'usia_pelanggan' => '6 bulan',
+                'no_hp_pelanggan' => '081136111112',
+                'kota_pelanggan' => 'Malang',
+                'status_member' => 0
+            ],
+            [
+                'nama_pelanggan' => 'Eshal',
+                'nama_ortu_pelanggan' => 'Irma Tri Andini',
+                'usia_pelanggan' => '10 bulan',
+                'no_hp_pelanggan' => '085636021118',
+                'kota_pelanggan' => 'Malang',
+                'status_member' => 1
+            ],
+            [
+                'nama_pelanggan' => 'Tamam',
+                'nama_ortu_pelanggan' => 'Bunda Lintang',
+                'usia_pelanggan' => '14 bulan',
+                'no_hp_pelanggan' => '089541395266',
+                'kota_pelanggan' => 'Malang',
+                'status_member' => 0
+            ],
+            [
+                'nama_pelanggan' => 'Hanum',
+                'nama_ortu_pelanggan' => 'Kinanti Nur',
+                'usia_pelanggan' => '17 bulan',
+                'no_hp_pelanggan' => '0895622732614',
+                'kota_pelanggan' => 'Malang',
+                'status_member' => 0
+            ]
+        ];
+
+        $this->db->table('m_pelanggan')->insertBatch($data);
+    }
+}
